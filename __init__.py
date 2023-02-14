260596a4f10713ccf6ae0c904f9615264cc5238e
winghavenrc <99767144+winghavenrc@users.noreply.github.com>
2023-02-14 11:24:10 -0600

    r

--- __init__.py
+++ __init__.py
@@ -1,32 +1,36 @@
+from appointments import find_first
 from mycroft import MycroftSkill, intent_file_handler
 
-#import os
-#import openai
+# import os
+# import openai
 
 import json
+import datetime
+import requests
 
-import appointments
+#import appointments
 
 
 class VoxHealth(MycroftSkill):
-    
- #   def initialize(self):
- #       self.register_entity_file('type.entity')
 
     def __init__(self):
         MycroftSkill.__init__(self)
 
+ #       self.register_entity_file('type.entity')
+
     @intent_file_handler('health.vox.intent')
-
     def handle_health_vox(self, message):
-#        self.speak_dialog('health.vox')
 
-        self.visit_types = ['health concern', 'wellness exam', 'vaccination', 'screening mammography']
+        self.log.info(message)
 
-        self.speak_dialog('visit.type', wait = True)
+        self.speak_dialog('health.vox')
 
+        self.visit_types = ['health concern', 'wellness exam',
+                            'vaccination', 'screening mammography']
 
-        visit_type = self.ask_selection(self.visit_types, min_conf = .4)
+        self.speak_dialog('visit.type', wait=True)
+
+        visit_type = self.ask_selection(self.visit_types, min_conf=.4)
 
 
 #        visit_type = self.get_response('visit.type')
@@ -40,16 +44,16 @@ class VoxHealth(MycroftSkill):
             self.log.info(self.file_system.path)
 
             with self.file_system.open('care_team.json', "r") as care_team_file:
-#            f = open('care_team.json')
-  
-# returns JSON object as 
-# a dictionary
+                #            f = open('care_team.json')
+
+                # returns JSON object as
+                # a dictionary
                 care_team = json.load(care_team_file)
 
 #            (data['people1'][0]
 # Iterating through the json
 # list
-     
+
     # for printing the key-value pair of
     # nested dictionary for loop can be used
 
@@ -70,21 +74,28 @@ class VoxHealth(MycroftSkill):
                 care_team_file.close()
 
                 self.log.info(self.provider_list)
-                self.speak_dialog("I can schedule with any of your currently active providers. Which one of these do you want to schedule with...", wait = True)
+                self.speak_dialog(
+                    "I can schedule with any of your currently active providers. Which one of these do you want to schedule with...", wait=True)
 
                 selected = self.ask_selection(self.provider_list)
-                self.speak_dialog('get.provider', data = {"provider": selected}, expect_response = True, wait=True)
+                self.speak_dialog('get.provider', data={
+                                  "provider": selected}, expect_response=True, wait=True)
 
 #               find first appointments available from today
-                timeSlots = appointments.find_first(self)
-                self.speak_dialog('speak.times', data = {"total": len(timeSlots)}, expect_response = False, wait=False)
-                for index in range(0,len(timeSlots)):
-                  self.speak_dialog('speak.timeslots', data = {"slot": timeSlots[index]["start"]}, expect_response = False, wait=False)
+                timeSlots = find_first(self)
+                self.speak_dialog('speak.times', data={"total": len(
+                    timeSlots)}, expect_response=False, wait=False)
+                for index in range(0, len(timeSlots)):
+                    self.speak_dialog('speak.timeslots', data={
+                                      "slot": timeSlots[index]["start"]}, expect_response=False, wait=False)
+
+
+
 
 
     def stop(self):
         pass
 
+
 def create_skill():
     return VoxHealth()
-
